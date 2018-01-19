de88e7096bf6ba8244dcd5005fc3ea84d788ab4e
Wade Ren <wr@3dsafety.com.au>
2018-01-19 16:54:06 +1100

    app core store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from '../shared/material/material.module';
+import { Routes, RouterModule } from '@angular/router';
+// import { MaterialModule } from '../shared/material/material.module';
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer
+} from '@ngrx/router-store';
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
-import { NgModule } from '@angular/core';
+import { reducers, effects, CustomSerializer } from './store';
 
-import { AppComponent } from './app.component';
+// not used in production
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+// this would be done dynamically with webpack for builds
+const environment = {
+  development: true,
+  production: false
+};
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+  ? [storeFreeze]
+  : [];
+
+import { AppComponent } from './containers/app/app.component';
+
+// routes
+export const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'tests' },
+  {
+    path: 'tests',
+    loadChildren: '../tests/tests.module#TestsModule'
+  }
+];
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot(effects),
+    StoreRouterConnectingModule,
+    environment.development ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
