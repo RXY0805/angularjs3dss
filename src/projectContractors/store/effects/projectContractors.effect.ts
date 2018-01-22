315cab701773fb6137d9709b3cbf43b8c6f903ec
Wade Ren <wr@3dsafety.com.au>
2018-01-22 13:09:37 +1100

    fix observable of issue

--- src/projectContractors/store/effects/projectContractors.effect.ts
+++ src/projectContractors/store/effects/projectContractors.effect.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
 import { map, switchMap, catchError } from 'rxjs/operators';
-// import { of } from 'rxjs/Observable/of';
+import { of } from 'rxjs/observable/of';
 
 import * as projectContractorActions from '../actions/projectContractors.action';
 import * as fromServices from '../../services';
@@ -20,21 +20,19 @@ export class ProjectContractorsEffects {
     .ofType(projectContractorActions.LOAD_PROJECTCONTRACTORS)
     .pipe(
       switchMap(() => {
-        return this.projectContractorService.getProjectContractors().pipe(
-          map(
-            projectContractors =>
-              new projectContractorActions.LoadProjectContractorsSuccess(
-                projectContractors
-              )
-          )
-          // catchError((error:any) => {
-
-          //   // return Observable.empty();
-          //   // Observable.of(
-          //   //   new projectContractorActions.LoadProjectContractorsFail(error)
-          //   // );
-          // })
-        );
+        return this.projectContractorService
+          .getProjectContractors()
+          .pipe(
+            map(
+              projectContractors =>
+                new projectContractorActions.LoadProjectContractorsSuccess(
+                  projectContractors
+                )
+            ),
+            catchError(error =>
+              of(new projectContractorActions.LoadProjectContractorsFail(error))
+            )
+          );
       })
     );
 }
