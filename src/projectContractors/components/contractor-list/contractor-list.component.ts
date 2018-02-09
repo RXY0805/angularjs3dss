14dff08a8f99e627bd1a55cacd3523fa64ed916b
Wade Ren <wr@3dsafety.com.au>
2018-02-09 18:48:11 +1100

    sort pagination filter working

--- src/projectContractors/components/contractor-list/contractor-list.component.ts
+++ src/projectContractors/components/contractor-list/contractor-list.component.ts
@@ -1,53 +1,60 @@
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/distinctUntilChanged';
-
-import { Component, Input, ViewChild } from '@angular/core';
-
-import { Observable } from 'rxjs/Observable';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-
-import { ProjectContractor } from '../../models/projectContractor.model';
-
-import { Contractor } from '../../models/contractor.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  ElementRef,
+  Input,
+  OnInit
+} from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
 import { Company } from '../../models/company.model';
-
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/filter';
+import { Subscription } from 'rxjs/Subscription';
+import { CompanyDatabase, CompanyDataSource } from './company.datasource';
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
 @Component({
   selector: 'app-contractor-list',
   templateUrl: './contractor-list.component.html',
-  styles: ['./contractor-list.component.css']
-  //changeDetection: ChangeDetectionStrategy.OnPush
+  styles: ['./contractor-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-// implements OnInit
-export class ContractorListComponent {
-  @Input() filteredContractors: Company[];
-
-  displayedColumns = ['id', 'name', 'email'];
-  dataSource: MatTableDataSource<Company>;
+export class ContractorListComponent implements OnInit {
+  @Input() filteredContractors: Observable<Company[]>;
+  private companyData: Company[] = [];
+  displayedColumns = ['id', 'name'];
+  public companyDatabase: CompanyDatabase;
+  public dataSource: CompanyDataSource;
+  public defaultPageSize: number;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  // selectedProjectId: number;
-  // selectedStatusId: number;
-  // isOnSite: boolean;
-  // isAuditStatus: boolean;
+  @ViewChild('filter') filter: ElementRef;
+  private paginatorSubscription: Subscription = Subscription.EMPTY;
+  private sortSubscription: Subscription = Subscription.EMPTY;
+  private query: string;
+  companysTrackByFn = (index: number, company: Company) => company.id;
 
-  //   @Input() projectStatus: ProjectStatus[];
-  //   @Input() auditStatus: AuditStatus[];
-  //   @Input() projectOnSiteStatus: ProjectOnSiteStatus[];
-
-  //   @Input() searching = false;
-  //   @Input() error = '';
-  //   @Output()
-  //     onSearch: EventEmitter<ContractorFilter> = new EventEmitter<
-  //       ContractorFilter
-  //     >();
-  constructor() {
-    this.dataSource = new MatTableDataSource(this.filteredContractors);
+  constructor() {}
+  ngOnInit() {
+    // this.defaultPageSize = this.isCheckable ? 5:10;
+    this.displayedColumns = ['id', 'name', 'email'];
+    this.companyDatabase = new CompanyDatabase(this.filteredContractors);
+    this.dataSource = new CompanyDataSource(
+      this.companyDatabase,
+      this.paginator,
+      this.sort
+    );
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) {
+          return;
+        }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      });
   }
-
-  // ngAfterViewInit() {
-  //   this.dataSource.paginator = this.paginator;
-  //   this.dataSource.sort = this.sort;
-  // }
 }
